54a12a6479616d247d96ce4df6d905e362d45f8e
Lucien Thomas <lthomas@sciport.org>
2024-05-27 21:52:55 -0500

    fix: fix login button

--- src/components/LoginWithSpotify.tsx
+++ src/components/LoginWithSpotify.tsx
@@ -29,20 +29,22 @@ function LoginWithSpotify() {
 
 function LoginLink() {
   return (
-    <Button className="rounded-[0.7rem] items-center flex justify-between w-full bg-black text-white">
-      <div style={{ flex: 1 }}>
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/800px-Spotify_logo_without_text.svg.png"
-          alt="Spotify Logo"
-          width="24"
-          height="24"
-        />
-      </div>
-      <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
-        <Link href="/api/login">Login</Link>
-      </div>
-      <div style={{ flex: 1 }}></div>
-    </Button>
+    <Link href="/api/login">
+      <Button className="rounded-[0.7rem] items-center flex justify-between w-full bg-black text-white">
+        <div style={{ flex: 1 }}>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/800px-Spotify_logo_without_text.svg.png"
+            alt="Spotify Logo"
+            width="24"
+            height="24"
+          />
+        </div>
+        <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
+          Login
+        </div>
+        <div style={{ flex: 1 }}></div>
+      </Button>
+    </Link>
   );
 }
 
