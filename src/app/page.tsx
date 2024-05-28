f0501d1c8346b338718b63b6cce653ead091c8e4
Lucien Thomas <lthomas@sciport.org>
2024-05-27 22:10:52 -0500

    test: fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,17 +26,22 @@ export default function Home() {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("access_token");
+    console.log("Token from URL:", token);
+
     if (token) {
       window.localStorage.setItem("accessToken", token);
       setAccessToken(token);
       window.history.replaceState(null, "", window.location.pathname);
     } else {
       const storedToken = window.localStorage.getItem("accessToken");
+      console.log("Token from localStorage:", storedToken);
       if (storedToken) {
         setAccessToken(storedToken);
       }
     }
+  }, []);
 
+  useEffect(() => {
     if (accessToken) {
       fetchUserData();
       fetchTopArtists();
