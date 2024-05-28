9699c2453fb2e1d49f2eda9b4294f383b8f4675b
Lucien Thomas <lthomas@sciport.org>
2024-05-27 22:06:02 -0500

    fix: test

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,15 +4,18 @@ import querystring from "querystring";
 export async function GET(req: NextRequest) {
   const scope = "user-read-private user-read-email user-top-read";
   const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI;
+  const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 
   const authUrl =
     "https://accounts.spotify.com/authorize?" +
     querystring.stringify({
       response_type: "code",
-      client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+      client_id: clientId,
       scope: scope,
       redirect_uri: redirectUri,
     });
+
+  console.log("Redirecting to Spotify auth URL:", authUrl);
 
   return NextResponse.redirect(authUrl);
 }
