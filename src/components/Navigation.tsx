e1daf4ab50fb879b4578338d2169ee038eef8eb6
Lucien Thomas <lthomas@sciport.org>
2024-05-31 18:07:14 -0500

    style: current route highlighted

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 export default function Navigation() {
   const router = useRouter();
+  const path = usePathname();
+
+  const isActive = (pathname: string) => {
+    return path === pathname;
+  };
 
   return (
     <>
-      <Button variant="link" onClick={() => router.push("/")}>
+      <Button
+        variant="link"
+        className={isActive("/") ? "text-foreground" : "text-muted-foreground"}
+        onClick={() => router.push("/")}
+      >
         Top Artists
       </Button>
-      <Button variant="link" onClick={() => router.push("/top-100-tracks")}>
+      <Button
+        variant="link"
+        className={
+          isActive("/top-100-tracks")
+            ? "text-foreground"
+            : "text-muted-foreground"
+        }
+        onClick={() => router.push("/top-100-tracks")}
+      >
         Top 100 Tracks
       </Button>
     </>
