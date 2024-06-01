0ab55ccd5759c988f2ccd75b102a0f710af5a66d
Lucien Thomas <lthomas@sciport.org>
2024-06-01 12:35:11 -0500

    feat: play 5 seconds of song

--- src/app/guess-the-song/page.tsx
+++ src/app/guess-the-song/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useAccessToken } from "@/hooks/useAccessToken";
 import { useTopTracksQuery } from "@/hooks/useTopTracksQuery";
 
@@ -13,6 +13,7 @@ import {
   CardDescription,
   CardFooter,
 } from "@/components/ui/card";
+import { Pause, Play } from "lucide-react";
 
 import type { Track } from "@/types/types";
 
@@ -28,6 +29,9 @@ export default function GuessTheSong() {
   const [randomTrack, setRandomTrack] = useState<Track | null>(null);
   const [randomTrackIndex, setRandomTrackIndex] = useState<number | null>(null);
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   useEffect(() => {
     if (topTracks && topTracks.length > 0) {
@@ -41,6 +45,8 @@ export default function GuessTheSong() {
     setRandomTrackIndex(index + 1); // +1 to make the position 1-based
     setTrackGuess("");
     setIsCorrect(null);
+    setIsPlaying(false);
+    setCurrentTime(0);
   };
 
   const handleGuessChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -63,7 +69,49 @@ export default function GuessTheSong() {
 
   const handleReset = () => {
     pickRandomTrack();
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
   };
+
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setCurrentTime(0);
+    }
+  }, [randomTrack]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      const audioElement = audioRef.current;
+      const handleTimeUpdate = () => {
+        setCurrentTime(audioElement.currentTime);
+        if (audioElement.currentTime >= 5) {
+          audioElement.pause();
+          audioElement.currentTime = 0;
+          setIsPlaying(false);
+        }
+      };
+
+      audioElement.addEventListener("timeupdate", handleTimeUpdate);
+      return () => {
+        audioElement.removeEventListener("timeupdate", handleTimeUpdate);
+      };
+    }
+  }, [audioRef, randomTrack]);
 
   if (isLoadingTopTracks) {
     return <div>Loading...</div>;
@@ -86,10 +134,32 @@ export default function GuessTheSong() {
             </CardHeader>
             <CardContent className="text-center">
               {randomTrack.preview_url && (
-                <audio controls>
-                  <source src={randomTrack.preview_url} type="audio/mpeg" />
-                  Your browser does not support the audio element.
-                </audio>
+                <div>
+                  <audio ref={audioRef} style={{ display: "none" }}>
+                    <source src={randomTrack.preview_url} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                  <div className="flex items-center justify-center">
+                    <Button
+                      onClick={handlePlayPause}
+                      className="rounded-[0.5rem] mt-2"
+                      variant="ghost"
+                    >
+                      {isPlaying ? <Pause size={20} /> : <Play size={20} />}
+                    </Button>
+                    <div className="mt-2 w-full flex items-center justify-center">
+                      <div className="w-full bg-gray-300 h-2 rounded">
+                        <div
+                          className="bg-blue-500 h-2 rounded"
+                          style={{ width: `${(currentTime / 5) * 100}%` }}
+                        ></div>
+                      </div>
+                      <span className="ml-2 text-sm">
+                        {Math.floor(currentTime)}/5s
+                      </span>
+                    </div>
+                  </div>
+                </div>
               )}
             </CardContent>
             <CardFooter className="flex flex-col space-y-4 justify-center items-center">
